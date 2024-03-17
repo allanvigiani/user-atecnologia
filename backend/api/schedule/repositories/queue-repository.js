7ad8667ec5afc3ece922b131dcc055e598b7bf12
Allan <allan.vigiani@smartnx.com>
2024-03-17 10:38:39 -0300

    fix: Corrigindo Dockerfile e docker-compose e retirando função de consumer do rabbitMq

--- backend/api/schedule/repositories/queue-repository.js
+++ backend/api/schedule/repositories/queue-repository.js
@@ -12,17 +12,6 @@ class QueueRepository {
         }
     }
 
-    async consumeQueue(queue) {
-        const channel = await connectRabbitMQ();
-        console.log(" [*] Aguardando por mensagens em %s.", queue);
-        channel.consume(queue, message => {
-            if (message !== null) {
-                console.log(" [x] Recebido '%s'", message.content.toString());
-                channel.ack(message);
-            }
-        });
-    }
-
 }
 
 export default QueueRepository;
